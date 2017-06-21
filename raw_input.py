6115b5d2b5c25e835e6bc42c0f3f00199ad23c45
Matt I <mimmerma@gmail.com>
2017-06-21 12:17:37 -0600

    write actual prompt for user input
    - decided to print an intro before the input prompt with the details of
    user input formats
    - kept the prompt itself short and sweet

--- raw_input.py
+++ raw_input.py
@@ -1,11 +1,32 @@
 #
 #
 #
-
 import rlcompleter
 
-# raw_input([prompt]) - no newline after [prompt] in output
-# returns user input
-s = raw_input('--> ')
-print("you entered: ", s)
+# intro
+print
+print('Welcome to Family Tree!')
+print('Please enter info to begin or continue building your tree, or to display it')
+print('Entries should be in the form [method], [<optional-arguments comma delimited>]')
+print('Possible methods are: "addPerson", "deathNotice", "displayTree"')
+print
+print('addPerson method: <name-of-person>, <birth-date>, <name-of-first-parent>, [<optional-name-of-second-parent>]')
+print('Ex: addPerson, Jason Immerman, 05/22/1988, Irene Immerman, Michael Immerman')
+print
+print('deathNotice method: <name-of-deceased>, [<optional-date-of-death>]')
+print('Ex: deathNotice, David Bowie, 01/10/2016')
+print('Note about deathNotice: must be called with name of existing Person object')
+print('Note about deathNotice: date-of-death is optional because might be unknown')
 
+print
+print('displayTree method takes no arguments')
+print('Ex: displayTree')
+print('\n')
+
+# prompt for user input in form described in intro
+userInput = raw_input('[method], [<optional-arguments-comma-delimited>] --> ')
+
+print('you entered: ', userInput)
+#
+#
+#
