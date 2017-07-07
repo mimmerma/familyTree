f0c87ea0efafdac1205f2c6d3538d5e740026de1
Matt I <mimmerma@gmail.com>
2017-07-07 12:07:29 -0600

    stored strings in variables and functionailze
    - stored message strings for help and input prompt in variables
    - functionalized displayHelp() and promptUser()

--- raw_input.py
+++ raw_input.py
@@ -3,30 +3,37 @@
 #
 import rlcompleter
 
-# intro
-print
-print('Welcome to Family Tree!')
-print('Please enter info to begin or continue building your tree, or to display it')
-print('Entries should be in the form [method], [<optional-arguments comma delimited>]')
-print('Possible methods are: "addPerson", "deathNotice", "displayTree"')
-print
-print('addPerson method: <name-of-person>, <birth-date>, <name-of-first-parent>, [<optional-name-of-second-parent>]')
-print('Ex: addPerson, Jason Immerman, 05/22/1988, Irene Immerman, Michael Immerman')
-print
-print('deathNotice method: <name-of-deceased>, [<optional-date-of-death>]')
-print('Ex: deathNotice, David Bowie, 01/10/2016')
-print('Note about deathNotice: must be called with name of existing Person object')
-print('Note about deathNotice: date-of-death is optional because might be unknown')
+displayHelp():
+	'''displays help message explaining user input format
+		\nprint message when 1. user requests OR 2. user enters invalid information'''
+	helpMessage = '''\nWelcome to Family Tree!
+						\nPlease enter info to begin or continue building your tree, or to display it
+						\nEntries should be in the form [method], [<optional-arguments comma delimited>]
+						\nPossible methods are: "addPerson", "deathNotice", "displayTree"
 
-print
-print('displayTree method takes no arguments')
-print('Ex: displayTree')
-print('\n')
+						\naddPerson method: <name-of-person>, <birth-date>, <name-of-first-parent>, [<optional-name-of-second-parent>]
+						\nEx: addPerson, Jason Immerman, 05/22/1988, Irene Immerman, Michael Immerman
 
-# prompt for user input in form described in intro
-userInput = raw_input('[method], [<optional-arguments-comma-delimited>] --> ')
+						\ndeathNotice method: <name-of-deceased>, [<optional-date-of-death>]
+						\nEx: deathNotice, David Bowie, 01/10/2016
+						\nNote about deathNotice: must be called with name of existing Person object
+						\nNote about deathNotice: date-of-death is optional because might be unknown
 
-print('you entered: ', userInput)
+						\ndisplayTree method takes no arguments
+						\nEx: displayTree
+						\n'''
+
+	print(helpMessage)
+
+promptUser():
+	'''prompt for user input in form described in intro'''
+	rawInputPrompt = '[method], [<optional-arguments-comma-delimited>] --> '
+	userInput = raw_input(rawInputPrompt)
+
+	print('you entered: ', userInput)
+
+displayHelp()
+promptUser()
 #
 #
 #
